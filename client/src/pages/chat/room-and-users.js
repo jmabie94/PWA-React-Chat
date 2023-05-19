48d441017938555431fca0e601b5e9049a69348f
Jack Mabie <johnmabie94@gmail.com>
2023-05-18 22:09:36 -0500

    ran prettier

--- client/src/pages/chat/room-and-users.js
+++ client/src/pages/chat/room-and-users.js
@@ -5,50 +5,50 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const RoomAndUsers = ({ socket, username, room }) => {
-    const [roomUsers, setRoomUsers] = useState([]);
-    const navigate = useNavigate();
+  const [roomUsers, setRoomUsers] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        socket.on('chatroom_users', (data) => {
-        console.log(data);
-        setRoomUsers(data);
-        });
+  useEffect(() => {
+    socket.on('chatroom_users', (data) => {
+      console.log(data);
+      setRoomUsers(data);
+    });
 
-        return () => socket.off('chatroom_users');
-    }, [socket]);
+    return () => socket.off('chatroom_users');
+  }, [socket]);
 
-    const leaveRoom = () => {
-        const __createdtime__ = Date.now();
-        socket.emit('leave_room', { username, room, __createdtime__ });
-        // Redirect to home page
-        navigate('/', { replace: true });
-    };
+  const leaveRoom = () => {
+    const __createdtime__ = Date.now();
+    socket.emit('leave_room', { username, room, __createdtime__ });
+    // Redirect to home page
+    navigate('/', { replace: true });
+  };
 
-    return (
-        <div className={styles.roomAndUsersColumn}>
-        <h2 className={styles.roomTitle}>{room}</h2>
+  return (
+    <div className={styles.roomAndUsersColumn}>
+      <h2 className={styles.roomTitle}>{room}</h2>
 
-        <div>
-            {roomUsers.length > 0 && <h5 className={styles.usersTitle}>Users:</h5>}
-            <ul className={styles.usersList}>
-            {roomUsers.map((user) => (
-                <li
-                style={{
-                    fontWeight: `${user.username === username ? 'bold' : 'normal'}`,
-                }}
-                key={user.id}
-                >
-                {user.username}
-                </li>
-            ))}
-            </ul>
-        </div>
+      <div>
+        {roomUsers.length > 0 && <h5 className={styles.usersTitle}>Users:</h5>}
+        <ul className={styles.usersList}>
+          {roomUsers.map((user) => (
+            <li
+              style={{
+                fontWeight: `${user.username === username ? 'bold' : 'normal'}`,
+              }}
+              key={user.id}
+            >
+              {user.username}
+            </li>
+          ))}
+        </ul>
+      </div>
 
-        <button className='btn btn-outline' onClick={leaveRoom}>
-            Leave
-        </button>
-        </div>
-    );
+      <button className="btn btn-outline" onClick={leaveRoom}>
+        Leave
+      </button>
+    </div>
+  );
 };
 
 export default RoomAndUsers;
--- client/src/pages/chat/send-message.js
+++ client/src/pages/chat/send-message.js
@@ -4,30 +4,30 @@ import styles from './styles.module.css';
 import React, { useState } from 'react';
 
 const SendMessage = ({ socket, username, room }) => {
-const [message, setMessage] = useState('');
+  const [message, setMessage] = useState('');
 
-const sendMessage = () => {
+  const sendMessage = () => {
     if (message !== '') {
-    const __createdtime__ = Date.now();
-    // Send message to server. We can't specify who we send the message to from the frontend. We can only send to server. Server can then send message to rest of users in room
-    socket.emit('send_message', { username, room, message, __createdtime__ });
-    setMessage('');
+      const __createdtime__ = Date.now();
+      // Send message to server. We can't specify who we send the message to from the frontend. We can only send to server. Server can then send message to rest of users in room
+      socket.emit('send_message', { username, room, message, __createdtime__ });
+      setMessage('');
     }
-};
+  };
 
-return (
+  return (
     <div className={styles.sendMessageContainer}>
-    <input
+      <input
         className={styles.messageInput}
-        placeholder='Message...'
+        placeholder="Message..."
         onChange={(e) => setMessage(e.target.value)}
         value={message}
-    />
-    <button className='btn btn-primary' onClick={sendMessage}>
+      />
+      <button className="btn btn-primary" onClick={sendMessage}>
         Send Message
-    </button>
+      </button>
     </div>
-);
+  );
 };
 
 export default SendMessage;
