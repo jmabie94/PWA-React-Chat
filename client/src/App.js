b8e590b3a6661a8aec0acf5ffcbeeed2e1be8084
Jack Mabie <johnmabie94@gmail.com>
2023-05-19 00:53:10 -0500

    continual attempts at refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
 // client/src/App.js
 
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Home from './pages/home';
 import Chat from './pages/chat';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import io from 'socket.io-client';
 
 // chatgpt refactor, heroku deployed but could not "GET '/'" route cause it wasn't local.
-const serverURL = process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:4000';
-const socket = io.connect(serverURL);
+// the secondary refactor replaces this with the refactor with the useState/useEffect io() logic.
+// const serverURL = process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:4000';
+// const socket = io.connect(serverURL);
 
 function App() {
   const [username, setUsername] = useState('');
   const [room, setRoom] = useState('');
+
+  // another chatgpt refactor, adding a useEffect to change the socket connection
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io();
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    }
+  }, []);
+  // chatgpt refactor ends here
 
   return (
     <Router>
